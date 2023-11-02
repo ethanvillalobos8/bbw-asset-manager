82adb1b925d4d4809e6fe9335487b156ab1fefbf
Ethan <ethan.villalobos8@gmail.com>
2023-11-02 09:27:08 -0500

    Revert commit

--- bbw-asset-portal/app/page.js
+++ bbw-asset-portal/app/page.js
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from '../utils/firebase';
+import { auth } from '/utils/firebase';
 
 const LoginPage = () => {
   const router = useRouter();
